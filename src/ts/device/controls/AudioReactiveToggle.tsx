c14e5944d2a0011f62dc488f71d607f612659e1f
Charlie Hendricks <charlie.hendricks@equipmentshare.com>
2020-06-04 22:00:21 -0500

    Added audio sensitivity to AudioReactive component

--- src/ts/device/controls/AudioReactiveToggle.tsx
+++ src/ts/device/controls/AudioReactiveToggle.tsx
@@ -2,23 +2,37 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, Switch, View, Text} from 'react-native';
 import {FunctionResult, VariableResult} from '../DeviceModels';
 import {useParticleAPI} from '../../../ParticleAPI';
-import { deviceStyle } from '../Device';
+import {deviceStyle} from '../Device';
+import Slider from '@react-native-community/slider';
 
 export function AudioReactiveToggle({id, accessToken}: any) {
   const [
-    {audioReactiveOn, isError, isLoading},
+    {audioReactiveOn, sensitivity, isError, isLoading},
     setAudioReactiveState,
+    setSensitivity
   ] = useAudioReactiveState(id, accessToken);
+
 
   const style = StyleSheet.create({
     container: {
       flex: 1,
-      flexDirection: 'row',
-      alignContent: 'center',
-      paddingLeft: 4
+      flexDirection: 'column',
+      paddingLeft: 4,
     },
-    switch: {
+    toggleContainer: {
       flex: 1,
+      flexDirection: 'row',
+    },
+    toggle: {
+      flex: 1,
+    },
+    sliderContainer: {
+      flex: 1,
+      paddingTop: 12
+    },
+    slider: {
+      flex: 1,
+      paddingTop: 8
     },
     text: {
       fontSize: 20,
@@ -28,16 +42,38 @@ export function AudioReactiveToggle({id, accessToken}: any) {
 
   return (
     <View style={style.container}>
-      <Text style={[style.text, deviceStyle.sectionTitle]}>Audio Reactive Mode</Text>
-      <Switch
-        style={style.switch}
-        trackColor={{false: '#767577', true: '#bfc0c0'}}
-        thumbColor={true ? '#ef8354' : '#2d3142'}
-        onValueChange={(value) => {
-          setAudioReactiveState(value);
-        }}
-        value={audioReactiveOn}
-      />
+      <View style={style.toggleContainer}>
+        <Text style={[style.text, deviceStyle.sectionTitle]}>
+          Audio Reactive Mode
+        </Text>
+        <Switch
+          style={style.toggle}
+          trackColor={{false: '#767577', true: '#bfc0c0'}}
+          thumbColor={true ? '#ef8354' : '#2d3142'}
+          onValueChange={(value) => {
+            setAudioReactiveState(value);
+          }}
+          value={audioReactiveOn}
+        />
+      </View>
+      <View style={style.sliderContainer}>
+        <Text
+          style={[
+            style.text,
+            deviceStyle.sectionTitle,
+          ]}>{`Sensitivity: ${sensitivity}`}</Text>
+        <Slider
+          style={style.slider}
+          value={sensitivity}
+          minimumValue={500}
+          maximumValue={2400}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+          onSlidingComplete={(speed: number) => {
+            setSensitivity(speed);
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -45,8 +81,9 @@ export function AudioReactiveToggle({id, accessToken}: any) {
 export const useAudioReactiveState = (
   deviceId: string,
   accessToken: string,
-): [any, CallableFunction] => {
+): [any, CallableFunction, CallableFunction] => {
   const [audioReactiveOn, setAudioReactiveOn] = useState<boolean>(false);
+  const [sensitivity, setInternalSensitivity] = useState<number>(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [{}, functionRequest, variableRequest] = useParticleAPI(
@@ -63,6 +100,7 @@ export const useAudioReactiveState = (
       try {
         if (mounted) {
           getAudioReactiveState();
+          getSensitivity();
         }
       } catch (error) {
         setIsError(true);
@@ -89,5 +127,21 @@ export const useAudioReactiveState = (
     });
   };
 
-  return [{audioReactiveOn, isError, isLoading}, setAudioReactiveState];
+  const getSensitivity = () => {
+    variableRequest('sensitivity').then((result: VariableResult) => {
+        if (!!result.result) {
+            setInternalSensitivity(parseInt(result.result));
+        }
+    });
+  };
+
+  function setSensitivity(speed: number) {
+    functionRequest('set-sensitivity', speed?.toString()).then(
+      (result: FunctionResult) => {
+        setInternalSensitivity(result.return_value);
+      },
+    );
+  }
+
+  return [{audioReactiveOn, sensitivity, isError, isLoading}, setAudioReactiveState, setSensitivity];
 };
